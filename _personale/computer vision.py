799fc1ca6e84bb3c0e60f331ac3e45b509a1eb73
michela-pe <167460368+michela-pe@users.noreply.github.com>
2024-05-14 17:46:31 +0000

    aggiunto file computer vision

---
+++ _personale/computer vision.py
@@ -0,0 +1,6 @@
+import cv2
+#webcam=cv2.VideoCapture(0) #apre la prima webcom del nostro computer (nel nostro caso il pc ha solo una webcam) 
+
+#while True: 
+   # ret, frame= webcam.read()  #ret=1 significa "riuscito", =0 significa "non riuscito". Frame è il fotogramma letto
+    #cv2.imshow("Immagini", frame)  #mostra il fotogramma
