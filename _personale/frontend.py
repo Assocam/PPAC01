10fd08d141175848fdabbe16d63f9475aacc9c6c
michela-pe <167460368+michela-pe@users.noreply.github.com>
2024-06-07 19:31:43 +0000

    mofdificato frontend

--- _personale/frontend.py
+++ _personale/frontend.py
@@ -2,10 +2,10 @@ import requests
 #import json
 
 datij={'username':'prova', 'password':'123'}
-#datij=json.dumps(datij)
 
 risposta=requests.get('http://127.0.0.1/datijson',
-                      data=datij)
+                      json=datij) # al posto del parametro json posso mettere data=json.dumps(datij)
+
 
 codice= risposta.status_code  #è il come mi ha risposto. E' un codice numerico
 
